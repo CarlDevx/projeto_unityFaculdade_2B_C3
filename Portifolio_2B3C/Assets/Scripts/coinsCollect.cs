c40db03ec7f7eae209e61323bf31cc555f70aa5b
Carlos Daniel C <carlosdanielcostadesousa02@gmail.com>
2024-05-14 17:16:01 -0300

    secure backup

--- Portifolio_2B3C/Assets/Scripts/coinsCollect.cs
+++ Portifolio_2B3C/Assets/Scripts/coinsCollect.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class coinsCollect : MonoBehaviour
-{   
-    void Update()
+{
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player")) {
+            Destroy(this);
+        }
     }
 }
--- Portifolio_2B3C/Assets/Scripts/player.cs
+++ Portifolio_2B3C/Assets/Scripts/player.cs
@@ -7,13 +7,17 @@ public class player : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
     [SerializeField]
     private float speed = 15f;
     private float horizontalAxis;
+    [SerializeField]
     private bool isOnFloor = false;
     [SerializeField]
     private float jumpForce = 30f;
     void Awake(){
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
@@ -22,8 +26,41 @@ public class player : MonoBehaviour
         if (Input.GetKey(KeyCode.Space) && isOnFloor){
             rb.AddForce(Vector2.up * jumpForce);
         }
+        switch (horizontalAxis)
+        {
+            case < 0:
+                spriteRenderer.flipX = true;
+                break;
+            case > 0:
+                spriteRenderer.flipX = false;
+                break;
+            default:
+                spriteRenderer.flipX = spriteRenderer.flipX;
+                break;
+        }
     }
     void FixedUpdate(){
         rb.velocity = new Vector2(horizontalAxis * speed, rb.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground")) {
+            isOnFloor = true;
+            animator.SetBool("idle",true);
+            animator.SetBool("jump", false);
+        }
+        if (collision.gameObject.CompareTag("coin"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            animator.SetBool("idle", false);
+            animator.SetBool("jump", true);
+            isOnFloor = false;
+        }
     }
 }
